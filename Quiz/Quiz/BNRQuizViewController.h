68501b336cbe1ea859bdf1b50e24323bc1bb070e
Raymon Atienza <raymon.atienza@gmail.com>
2014-05-04 23:10:17 -0400

    Added project files, and completed Chapter 1

---
+++ Quiz/Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Raymon Atienza on 2014-05-04.
+//  Copyright (c) 2014 Raymon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ Quiz/Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Raymon Atienza on 2014-05-04.
+//  Copyright (c) 2014 Raymon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
